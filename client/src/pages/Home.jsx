028ebb39c06ed90874815edb9150e6d1f0752e0c
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2023-01-24 19:27:50 +0200

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { Card, FormField, Loader } from "../components";
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home;

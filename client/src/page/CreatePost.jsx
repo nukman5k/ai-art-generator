56fbb9eef022d99574be926f2d0769421755a2a4
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2023-02-15 16:34:03 +0200

    update

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://ai-art-generator-vek5.onrender.com', {
+        const response = await fetch('https://ai-art-generator-vek5.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
